ad66226201c4a38fc5963d047a6d15ca20651877
Lucana <k.ark.scarlet72@gmail.com>
2023-07-08 17:13:06 +0900

    [Finish]React.jsの学習用にmy-appを作成。index.html上にてボタンをクリックすると名前が変わるプログラムを実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: 'Michael'};
+  }
+
+  handleClickButton() {
+    this.setState({name: 'Luca'});
+  }
+
+  // handleClickButton() {
+  //   this.setState({});
+  // }
+
+  render() {
+    return (
+      // // デフォルト記述(React.jsのウェルカムページ)
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+      <div>
+        <h1>Hello world! This is {this.state.name}!</h1>
+        <button id="track-button" onClick={() => {this.handleClickButton()}}>Click Me!!</button>
+      </div>
+    );
+  }
 }
 
 export default App;
